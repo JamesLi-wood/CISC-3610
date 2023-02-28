65d177c00b21c977394e1ad1c1f6235c64ec033f
JamesLi-wood <jamesliwood@gmail.com>
2023-02-27 21:52:26 -0500

    added grass

--- hw1/hw1.js
+++ hw1/hw1.js
@@ -17,6 +17,44 @@ ctx.arc(440, 70, 30, 0, Math.PI * 2);
 ctx.arc(480, 70, 30, 0, Math.PI * 2);
 ctx.fill();
 
+for (let i = 120; i < canvas.height; i = i + 20) { // rain
+    for (let j = 205; j < 500; j = j + 10) {
+        ctx.beginPath();
+        ctx.moveTo(j, i);
+        ctx.lineTo(j,i + 10)
+        ctx.strokeStyle = "blue";
+        ctx.stroke(); 
+    }    
+}
+
+ctx.beginPath(); // lightning
+ctx.moveTo(220, 100);
+let posX = 220;
+let posY = 100;
+for(let i = 0; i < 5; i++) {
+    posX = posX + 40;
+    ctx.lineTo(posX, posY);
+    posX = posX - 20;
+    posY = posY + 60;
+    ctx.lineTo(posX, posY);
+}
+posY = posY - 40;
+console.log(posY);
+ctx.lineTo(posX, posY);
+posX = posX - 45;
+ctx.lineTo(posX, posY);
+console.log(posX, posY);
+for(let i = 0; i < 4; i++) {
+    posX = posX + 15;
+    posY = posY - 60;
+    ctx.lineTo(posX, posY);
+    posX = posX - 35;
+    ctx.lineTo(posX, posY);
+}
+ctx.closePath();
+ctx.fillStyle = "yellow";
+ctx.fill();
+
 ctx.fillStyle = "red"; // house
 ctx.fillRect(0, 350, 200, 150);
 
@@ -57,6 +95,7 @@ ctx.fillRect(0, canvas.height - 100, canvas.width, canvas.height);
 
 ctx.fillStyle = "white"; // fence-lining
 ctx.fillRect(220, 470, 360, 10);
+
 for (let i = 220; i < canvas.width; i = i + 50) { // fence picket
     ctx.beginPath();
     ctx.moveTo(i, canvas.height - 100);
@@ -65,7 +104,15 @@ for (let i = 220; i < canvas.width; i = i + 50) { // fence picket
     ctx.lineTo(i + 10, canvas.height - 150);
     ctx.lineTo(i + 10, canvas.height - 100);
     ctx.closePath();
-    ctx.stroke();
     ctx.fillStyle = "gray";
     ctx.fill();
+}
+
+for (let i = 200; i < canvas.width; i = i + 5) { // grass
+    ctx.beginPath();
+    ctx.moveTo(i, 500);
+    ctx.lineTo(i + 3, 490);
+    ctx.strokeStyle = "green";
+    ctx.stroke();
+    ctx.lineWidth = 3;
 }
